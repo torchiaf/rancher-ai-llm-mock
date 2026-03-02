a22a0b7ec4f45596581fd4202c4905712ce164ba
Francesco Torchia <francesco.torchia@suse.com>
2026-03-02 14:05:22 +0100

    temp
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,29 +1,10 @@
 package types
 
-import "encoding/json"
-
 type Text struct {
 	Chunks []string `json:"chunks"`
 }
 
-type Args []interface{}
-
-// UnmarshalJSON implements custom unmarshaling logic for Args to handle both array and single object formats
-func (a *Args) UnmarshalJSON(data []byte) error {
-	var arr []interface{}
-	if err := json.Unmarshal(data, &arr); err == nil {
-		*a = arr
-		return nil
-	}
-
-	// If that fails, unmarshal as single object and wrap in array
-	var single interface{}
-	if err := json.Unmarshal(data, &single); err != nil {
-		return err
-	}
-	*a = []interface{}{single}
-	return nil
-}
+type Args interface{}
 
 type Tool struct {
 	Name string `json:"name"`
